e677fb64f1b9c205194124b2847cc9c0e73637b0
bles.0xeth <105569585+blessingblockchain@users.noreply.github.com>
2023-04-29 16:24:36 +0100

    Delete AdvancedSec.sol

--- contracts/AdvancedSec.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
-contract MyContract is ReentrancyGuard {
-  function vulnerableFunction() public nonReentrant {
-    // Code vulnerable to reentrancy attacks
-  }
-}
